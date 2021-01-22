d8c21a57d79701b388a46a8084b013391e5627d5
Yauheni Yafimenka <yafimenka.jk@gmail.com>
2021-01-22 14:07:09 +0300

    init: deploy heroku

---
+++ frontend/dist/precache-manifest.8be75def0fb8c10c4de6965bc3055fad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcf034ff7f63bc8da5d1",
+    "url": "/css/app.12e5dc55.css"
+  },
+  {
+    "revision": "c903dbd7d26d4802e685",
+    "url": "/css/chunk-vendors.64d43e69.css"
+  },
+  {
+    "revision": "bde3fef343d165c8569ad6d45f0745dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcf034ff7f63bc8da5d1",
+    "url": "/js/app.a2898069.js"
+  },
+  {
+    "revision": "c903dbd7d26d4802e685",
+    "url": "/js/chunk-vendors.0fb2f8a1.js"
+  },
+  {
+    "revision": "a224424945aae13ec6d5ab13a844d543",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
