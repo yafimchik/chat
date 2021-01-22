6ce404188591bbee074ba2311a47183031b65663
Yauheni Yafimenka <yafimenka.jk@gmail.com>
2021-01-22 13:58:49 +0300

    init: deploy dist to server

---
+++ frontend/dist/precache-manifest.3923b018d8be7445d3c54f1e97d81dc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61b6c253dd8de5cac345",
+    "url": "/css/app.12e5dc55.css"
+  },
+  {
+    "revision": "c903dbd7d26d4802e685",
+    "url": "/css/chunk-vendors.64d43e69.css"
+  },
+  {
+    "revision": "3acbcd33d7c3ec34668a877c019242db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61b6c253dd8de5cac345",
+    "url": "/js/app.46ee5ca8.js"
+  },
+  {
+    "revision": "c903dbd7d26d4802e685",
+    "url": "/js/chunk-vendors.0fb2f8a1.js"
+  },
+  {
+    "revision": "a224424945aae13ec6d5ab13a844d543",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
